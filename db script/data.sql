9fdd7046556b9d40772643fab7d6ed6285f7b59a
jovanrt30 <jovanrt30@gmail.com>
2022-04-07 23:44:52 +0700

    update db script
    query drop table dan insert data

---
+++ db script/data.sql
@@ -0,0 +1,3 @@
+delete from course;
+insert into course (coursename, coursedesc, courseduration, imageurl)
+values ('Technical Analysis', 'Mempelajari cara membaca teknikal grafik dan pola-pola berulang yang seringkali muncul pada saat membaca grafik candlestick', '00:30:00', 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
--- db script/table.sql
+++ db script/table.sql
@@ -1,10 +1,13 @@
+drop table course;
 create table course(
     courseid serial primary key,
     coursename character varying(50),
     coursedesc character varying(500),
     courseduration time without time zone,
+	imageUrl character varying(500)
 );
 
+drop table course_enroll;
 create table course_enroll(
     userid integer NOT NULL,
     courseid integer NOT NULL,
@@ -12,7 +15,7 @@ create table course_enroll(
     CONSTRAINT courseenroll_pkey PRIMARY KEY (userid, courseid)
 );
 
-
+drop table quiz;
 create table quiz(
 	quizid serial primary key,
 	courseId int,
@@ -20,6 +23,7 @@ create table quiz(
 	quizdesc character varying(500)
 );
 
+drop table quiz_enroll;
 create table quiz_enroll(
     userid integer NOT NULL,
     quizid integer NOT NULL,
@@ -29,6 +33,7 @@ create table quiz_enroll(
     CONSTRAINT quizenroll_pkey PRIMARY KEY (userid, quizid, attemptno)
 );
 
+drop table question;
 create table question(
 	questionid serial primary key,
 	stepno int not null,
@@ -44,6 +49,7 @@ create table question(
 	imageUrl character varying(500)
 );
 
+drop table activity;
 create table activity(
 	activityid serial primary key,
 	courseid int,
@@ -53,11 +59,13 @@ create table activity(
 	imageUrl character varying(500)
 );
 
+drop table activity_type;
 create table activity_type(
 	id serial primary key,
 	name character varying(100)
 );
 
+drop table coin;
 CREATE TABLE coin
 (
     coinid serial NOT NULL PRIMARY KEY,
@@ -67,6 +75,7 @@ CREATE TABLE coin
     active VARCHAR(1)
 );
 
+drop table comment;
 CREATE TABLE comment
 (
     commentid SERIAL NOT NULL PRIMARY KEY,
@@ -76,6 +85,7 @@ CREATE TABLE comment
     topicid integer
 );
 
+drop table investmentplanning;
 CREATE TABLE investmentplanning
 (
     planningid SERIAL NOT NULL PRIMARY KEY,
@@ -86,12 +96,14 @@ CREATE TABLE investmentplanning
     currentbalance integer
 );
 
+drop table roles;
 CREATE TABLE roles
 (
     roleid integer NOT NULL PRIMARY KEY,
     rolename VARCHAR(10)
 );
 
+drop table topic;
 CREATE TABLE topic
 (
     topicid SERIAL NOT NULL PRIMARY KEY,
@@ -101,7 +113,8 @@ CREATE TABLE topic
     authorid integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.users
+drop table users;
+CREATE TABLE public.users
 (
     userid SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
