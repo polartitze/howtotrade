1418cdd01e24a846a122f1104174da7f50956eec
jovanrt30 <jovanrt30@gmail.com>
2022-04-09 08:57:32 +0700

    pindahin main

--- src/main/java/com/skripsi/howtotrade/utility/ServletInitializer.java
+++ src/main/java/com/skripsi/howtotrade/utility/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.skripsi.howtotrade.utility;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.skripsi.howtotrade.HowtotradeApplication;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
