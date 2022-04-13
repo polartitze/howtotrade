151824e881592d3eaedfdd7fa015389f0d0ef660
BryanJayanto <bryan.jayanto@yahoo.com>
2022-04-13 20:29:00 +0700

    Update NAVBAR OK
    Update NAVBAR OK, tapi LOGOUT perlu tambahan dari Alvin untuk PUSH

--- db script/data.sql
+++ db script/data.sql
@@ -12,9 +12,7 @@ values
 (1, 1, 'Technical Analysis', 'Mempelajari cara membaca teknikal grafik dan pola-pola berulang yang seringkali muncul pada saat membaca grafik candlestick', 
 	'https://images.unsplash.com/photo-1531913223931-b0d3198229ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80'),
 (2, 2, 'Fundamental Analysis', 'Mengetes kemampuan pengetahuan mengenai cara membaca laporan keuangan dan kesehatan suatu perusahaan', 
-	'https://images.unsplash.com/photo-1531913223931-b0d3198229ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80'),
-(3, 1, 3, 3, 'Saham apakah ini?', 2, 'BSML', 'DEWA', 'TAMA', 'GOTO',  
-	'https://facsekuritas.co.id/storage/media/original/dewa.jpg');
+	'https://images.unsplash.com/photo-1531913223931-b0d3198229ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80');
 	
 delete from question;
 
@@ -23,4 +21,6 @@ values
 (1, 1, 1, 1, 'Quiz 1 Question 1', 1, null, 'Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 
 	'https://canopylab.com/wp-content/uploads/2020/05/Working-with-adaptive-quizzes-A-beginners-guide.jpg'),
 (2, 1, 2, 2, 'Quiz 1 Question 2', 1, null, 'Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 
-	'https://canopylab.com/wp-content/uploads/2020/05/Working-with-adaptive-quizzes-A-beginners-guide.jpg');
+	'https://canopylab.com/wp-content/uploads/2020/05/Working-with-adaptive-quizzes-A-beginners-guide.jpg'),
+(3, 1, 3, 3, 'Saham apakah ini?', 2, 'BSML', 'DEWA', 'TAMA', 'GOTO',  
+	'https://facsekuritas.co.id/storage/media/original/dewa.jpg');
